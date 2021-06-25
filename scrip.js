4efffde98f50e0397d0afcf8b2a410a6ae4cdc06
Munsa1 <81932478+Munsa1@users.noreply.github.com>
2021-06-25 16:18:43 +0200

    Array sum

---
+++ scrip.js
@@ -0,0 +1,9 @@
+
+    var array = [1, 2, 3, 4, 5];
+    
+    // Getting sum of numbers
+    var sum = array.reduce(function(a, b){
+        return A + b;
+    }, 0);
+    
+    console.log(sum); // Prints: 15
